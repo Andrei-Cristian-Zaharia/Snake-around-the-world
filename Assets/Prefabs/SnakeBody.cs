cb269b2ddc728d970d2f634ca0cedf97159b2e03
Asar <zaharia.andrei2010@gmail.com>
2022-02-05 21:28:36 +0200

    Backup #1

---
+++ Assets/Prefabs/SnakeBody.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeBody : MonoBehaviour
+{
+    private void OnDestroy()
+    {
+        SnakeController SC = (SnakeController)FindObjectOfType(typeof(SnakeController));
+
+        SC.currentSize--;
+    }
+}
---
+++ Assets/Prefabs/SnakeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour
+{
+    public GameObject Head;
+
+    public int size = 0;
+
+    public int currentSize = 0;
+
+    public float distance = 0.25f;
+
+    public GameObject BodyPrefab;
+
+    private void Update()
+    {
+        while (currentSize < size)
+            StartCoroutine("GenerateTail");
+    }
+
+    IEnumerator GenerateTail()
+    {
+        yield return new WaitForSeconds(1);
+
+        Vector3 spawnPosition = new Vector3(Head.transform.position.x, Head.transform.position.y, Head.transform.position.z - distance);
+
+        GameObject newBody = Instantiate(BodyPrefab, spawnPosition, Quaternion.identity, this.transform) as GameObject;
+
+        currentSize++;
+
+        Destroy(newBody, 1);
+    }
+}
