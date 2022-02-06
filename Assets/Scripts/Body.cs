1b29d832e899320c827f6825b04ca78046719d11
Asar <zaharia.andrei2010@gmail.com>
2022-02-06 11:24:10 +0200

    Done
    - Removed usless files
    - Adjust scene
    - Adjust player proprieties

--- Assets/Scripts/Body.cs
+++ Assets/Scripts/Body.cs
@@ -5,11 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class Body : MonoBehaviour
 {
+    private bool canInteract;
+
+    private void Start()
+    {
+        StartCoroutine("EnableThis");
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && canInteract)
         {
             SceneManager.LoadScene("SampleScene");
         }
+    }
+
+    IEnumerator EnableThis()
+    {
+        yield return new WaitForSeconds(.5f);
+
+        canInteract = true;
     }
 }
